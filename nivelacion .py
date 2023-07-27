de737c87c45f83e519e820001f01507210f8bd80
JuanbarraSe <123966323+JuanbarraSe@users.noreply.github.com>
2023-07-27 08:49:31 -0500

    Add files via upload

---
+++ nivelacion .py
@@ -0,0 +1,51 @@
+print("Bienvenido a " )
+print("""                    ###                                  ##      ###      ###
+  #####    ####      ##      ####             ##  ##              ##       ##      ####     #####
+ ##       ##  ##     ##     ##  ##            ######    ###       ##       ##     ##  ##   ##
+  ####    ##  ##     ##     ##  ##            ######     ##       ##       ##     ##  ##    ####
+     ##   ##  ##     ##     ##  ##            ##  ##     ##       ##       ##     ##  ##       ##
+ #####     ####     ####     ####             ##  ##    ####     ####     ####     ####    #####
+
+
+""")
+#solicitar nombre
+nombre = input("Por favor ingresa tu nombre: ")
+
+print("Hola ", nombre, ", bienvenido a SOLO MILLOS.")
+print()
+#calcular la edad
+años = int(input("Para crear tu perfil, por favor ingresa en que año naciste  "))
+fecha_actual=2023
+edad = fecha_actual - años 
+print("Tu edad es:", edad) 
+#ciudad
+ciudad = input("En que ciudad vives: ")
+print("Tu ciudad es", ciudad,)
+#para terminar escribir los datos recolectados
+print("Gracias,", nombre, ". ahora crearemos un perfil con tus datos.")
+print("--------------------------------------------------")
+print("Nombre:  ", nombre)
+print("Edad:    ", edad, "años")
+print("ciudad: ", ciudad)
+print("Gracias por tu informacion,  que  disfrutes de SOLO MILLOS ")
+mensaje = input("Ahora puedes escribir tu primer mensaje. ¿como estas hoy?")
+print("--------------------------------------------------")
+print(nombre, "gracias por contarnos mas de ti; el dice", mensaje)
+print("--------------------------------------------------")
+print("bien ahora   queremos saber mas de ti")
+barrio = input(" ¿en que barrio vives?: ")
+print("Tu barrio es:", barrio)
+sexo = input(" Por favor ingresa tu sexo")
+print("tu sexo es  :", sexo)
+millos = input("¿apoyas a millos?: ")
+print("apoyas a millos:", millos )
+print("GRACIAS POR ESTAR EN")
+print("""###                                  ##      ###      ###
+  #####    ####      ##      ####             ##  ##              ##       ##      ####     #####
+ ##       ##  ##     ##     ##  ##            ######    ###       ##       ##     ##  ##   ##
+  ####    ##  ##     ##     ##  ##            ######     ##       ##       ##     ##  ##    ####
+     ##   ##  ##     ##     ##  ##            ##  ##     ##       ##       ##     ##  ##       ##
+ #####     ####     ####     ####             ##  ##    ####     ####     ####     ####    #####
+
+""")
+print(" adios, que estes bien")
